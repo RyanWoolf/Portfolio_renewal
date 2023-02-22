ccb7a0199fbe8c8383e28e1d4414004500e63ff0
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-02-22 21:54:41 +1000

    Modal added on each project contents

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Modal } from 'flowbite';
 import { Link } from 'react-router-dom'
 
 const Projects = () => {
@@ -87,22 +88,71 @@ const Projects = () => {
     }
   ]
 
+
   const projectsCards = projects.map((project, idx) => {
+    const $targetEl = document.getElementById(`popup-modal${idx}`);
+
+    // options with default values
+    const options = {
+      placement: 'bottom-right',
+      backdrop: 'dynamic',
+      backdropClasses: 'bg-main-1 bg-opacity-8 fixed inset-0 z-40',
+      closable: true,
+      onHide: () => {
+          console.log('modal is hidden');
+      },
+      onShow: () => {
+          console.log('modal is shown');
+      },
+      onToggle: () => {
+          console.log('modal has been toggled');
+      }
+    };
+    const modal = new Modal($targetEl, options);
+
       return (
             
         <div id={project.img} key={idx} className="flex flex-col snap-center bg-stone-600 bg-blend-multiply h-full  bg-cover bg-center min-w-full tablet:min-w-[50%] tablet:snap-start desktop:min-w-[25%] desktop:snap-start desktop:max-h-[700px]" 
           style={{backgroundImage: `url(${project.img})`}}>
           <div className="w-full min-w-fit h-full p-8 relative">
-            <h2 className="text-lg font-bold desktop:text-xl">{project.name}</h2>
-            <h3 className="text-xs opacity-70 desktop:text-base">{project.date}</h3>
-            <p className="text-xs  desktop:text-base">{project.stack}</p>
-            <div className="z-1 w-full flex justify-center items-center absolute bottom-1/2 left-0 transition duration-500 opacity-0 hover:opacity-100">
-              <a className="w-14 h-14 rounded-full bg-sub-2/[0.5] flex justify-center items-center " href={project.link} target="_blank">
+            <h2 className="text-lg font-bold desktop:text-xl opacity-90">{project.name}</h2>
+            <h3 className="text-xs opacity-60 font-semibold  desktop:text-base">{project.date}</h3>
+            <div className="z-1 w-full flex justify-center items-center absolute bottom-1/2 left-0 transition duration-500 opacity-10 hover:opacity-100">
+            {/* Modal button */}
+              <button data-modal-target={`popup-modal${idx}`}  data-modal-toggle={`popup-modal${idx}`}  className="block text-white bg-sub-2 opacity-80 hover:bg-sub-2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-3 py-3 text-center" type="button">
                 <svg className="opacity-70" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="24" height="24" viewBox="0 0 24 24">
                   <path d="M13 10h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2zm8.172 14l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7z"/></svg>
-              </a>
+              </button>
             </div>
-            <p className="origin-[0] w-max absolute bottom-5 pt-4 text-xs rotate-[270deg]  desktop:text-base">{project.desc}</p>
+            {/* Modal content start */}
+            <div id={`popup-modal${idx}`} tabIndex="-1" className="fixed top-1/3 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto h-modal">
+              <div className="relative w-full h-full max-w-md">
+                <div className="relative bg-gray-700 rounded-lg shadow">
+                  <div className="flex flex-col ">
+                    <div className="flex items-center justify-between p-4 border-b border-gray-600 rounded-t">
+                      <div className="flex flex-col">
+                        <h3 className="text-base -mb-0.5 font-semibold text-gray-300 desktop:text-lg">{project.name}</h3>
+                        <h3 className="text-xs opacity-60 desktop:text-xs">{project.date}</h3>
+                      </div>
+                      <button type="button" className=" duration-300 top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide={`popup-modal${idx}`} >
+                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                        <span className="sr-only">Close modal</span>
+                      </button>
+                    </div>
+                    <div className="p-4 border-b border-gray-600 rounded-t">
+                      <p className="mb-1 text-xs text-gray-400 desktop:text-sm">{project.stack}</p>
+                      <p className="my-1 text-xs text-gray-300 desktop:text-sm">{project.desc}</p>
+                    </div>
+                    <button data-modal-hide={`popup-modal${idx}`}  type="button" className="text-gray-300 w-20 ml-4 mb-4 duration-300 bg-sub-1 hover:drop-shadow-sub1 focus:ring-4 focus:outline-none focus:ring-gray-400 rounded-lg text-sm font-semibold px-4 py-1.5 mt-3 focus:z-10">
+                      <a target="_blank" href={project.link} className="h-full w-full" >
+                        View
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Modal content end */}
           </div>
         </div>
       )
